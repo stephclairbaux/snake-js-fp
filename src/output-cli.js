e76b1707663f1c80caa7b704d20bb8b9b4e33286
Stéphane Clairbaux <stephane@clairbaux.com>
2020-12-08 21:09:19 +0100

    add colors

--- src/output-cli.js
+++ src/output-cli.js
@@ -1,10 +1,11 @@
 import R from 'ramda'
+import chalk from 'chalk'
 import { createMatrix } from './matrix.js'
 
 const hr = (n) => Array(n).fill('\u2500').join('')
 const top = (n) => '\u250C' + hr(n) + '\u2510'
 const bottom = (n) => '\u2514' + hr(n) + '\u2518'
-const cellToStr = (c) => c === 2 ? '\u2588' : c === 1 ? '\u25EF' : ' '
+const cellToStr = (c) => c === 2 ? chalk.green('\u2588') : c === 1 ? chalk.red('\u2588') : ' '
 const lineToStr = (l) => ['\u2502', ...l.map(cellToStr), '\u2502'].join('')
 
 const matrixToStr = (matrix) => {
